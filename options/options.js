7d4b0057b4532ad5485e468091669b9c60702fb3
Ana Maria Franco <anafranco@microsoft.com>
2023-04-15 20:46:58 -0500

    querying chatgpt

--- options/options.js
+++ options/options.js
@@ -1,10 +1,35 @@
 // Saves options to chrome.storage
 const saveOptions = () => {
+  const status = document.getElementById("status");
   const apiKey = document.getElementById("apiKey").value;
   const endpoint = document.getElementById("endpoint").value;
   const deployment = document.getElementById("deployment").value;
   const maxTokens = document.getElementById("maxTokens").value;
   const temperature = document.getElementById("temperature").value;
+  const timeout = 3000;
+
+  if (
+    isNaN(temperature) ||
+    Number(temperature) < 0 ||
+    Number(temperature) > 1
+  ) {
+    status.textContent = "Temperature must be a number between 0 and 1.";
+    setTimeout(() => {
+      status.textContent = "";
+    }, timeout);
+    return;
+  }
+
+  if (
+    isNaN(maxTokens) ||
+    parseInt(maxTokens) < 0 
+  ) {
+    status.textContent = "The Max Tokens must be a number bigger than 0";
+    setTimeout(() => {
+      status.textContent = "";
+    }, timeout);
+    return;
+  }
 
   chrome.storage.sync.set(
     {
@@ -16,11 +41,10 @@ const saveOptions = () => {
     },
     () => {
       // Update status to let user know options were saved.
-      const status = document.getElementById("status");
       status.textContent = "Options saved.";
       setTimeout(() => {
         status.textContent = "";
-      }, 750);
+      }, timeout);
     }
   );
 };
@@ -42,10 +66,10 @@ const restoreOptions = () => {
       document.getElementById("maxTokens").value = items.maxTokens;
       document.getElementById("temperature").value = items.temperature;
 
-      for(option of document.getElementById("deployment").options){
-        if(option.value === items.deployment){
-            option.selected = true;
-            break;
+      for (option of document.getElementById("deployment").options) {
+        if (option.value === items.deployment) {
+          option.selected = true;
+          break;
         }
       }
     }
--- popup/popup.js
+++ popup/popup.js
@@ -3,21 +3,148 @@ const wordCountMessage = document.querySelector("#wordCount");
 const readTimeMessage = document.querySelector("#readTime");
 const summaryMessage = document.querySelector("#summary");
 const wordMatchRegExp = /[^\s]+/g;
+const chatContext =
+  "You are a reader assistant that provides summaries for articles.";
+const chatQuery =
+  "Provide a summary of the text below that captures its main idea.";
+const contextTokens = 100;
+const maxTokensMap = {
+  "code-davinci-002": 8001,
+  "text-davinci-003": 4097,
+  "gpt-3.5-turbo-0301": 4096,
+  "gpt-3.5-turbo": 4096,
+  "gpt-35-turbo-0301": 4096,
+  "gpt-4-0314": 8192,
+  "gpt-4-32k-0314": 8192,
+};
 
-button.addEventListener("click", async () => {
-  wordCountMessage.textContent = "Generating summary...";
-
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {}, function (response) {
-      if (response && response.article) {
-        const words = response.text.matchAll(wordMatchRegExp);
-        const wordCount = [...words].length;
-        const readingTime = Math.round(wordCount / 200);
-        wordCountMessage.textContent = `The article 📄 has ${wordCount} words.`;
-        readTimeMessage.textContent = `It takes ⏱️ ${readingTime} min to read it.`;
-      } else {
-        wordCountMessage.textContent = "No article in this page";
-      }
-    });
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  chrome.tabs.sendMessage(tabs[0].id, {}, function (response) {
+    if (response && response.article) {
+      fillInfo(response.text);
+    } else {
+      wordCountMessage.textContent = "No article in this page";
+    }
   });
 });
+
+function fillInfo(articleText) {
+  const words = articleText.matchAll(wordMatchRegExp);
+  const wordCount = [...words].length;
+  const readingTime = Math.round(wordCount / 200);
+  wordCountMessage.textContent = `The article 📄 has ${wordCount} words.`;
+  readTimeMessage.textContent = `It takes ⏱️ ${readingTime} min to read it.`;
+  getConfig((config) => {
+    summaryMessage.textContent = `Generating summary...`;
+    queryOpenAI(articleText, config, (response) => {
+      summaryMessage.textContent = response;
+    });
+  });
+}
+
+function getConfig(sendResponse) {
+  chrome.storage.sync.get(
+    {
+      apiKey: "",
+      endpoint: "",
+      deployment: "",
+      maxTokens: "0",
+      temperature: "0",
+    },
+    (items) => {
+      sendResponse(items);
+    }
+  );
+}
+
+function queryOpenAI(text, config, sendResponse) {
+  buildQuery(text, config)
+    .then(checkStatus)
+    .then((response) => response.json())
+    .then((jsonData) => sendResponse(processOpenAIResponse(jsonData)))
+    .catch((error) => sendResponse(error));
+}
+
+function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return Promise.resolve(response);
+  } else {
+    return Promise.reject(new Error(response.statusText));
+  }
+}
+
+function getMaxTokens(text, deployment) {
+  const words = text.matchAll(wordMatchRegExp);
+  const wordCount = [...words].length;
+  const maxTokens = maxTokensMap[deployment] - contextTokens - Math.round(wordCount * 1.5);
+  return maxTokens > 0 ? maxTokens : 100;
+}
+
+function queryOpenAICompletion(text, config) {
+  const data = {
+    prompt: `${chatContext}\n${chatQuery}\n${text}`,
+    max_tokens: getMaxTokens(text, config.deployment),
+    temperature: Number(config.temperature),
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    top_p: 1,
+    best_of: 1,
+    stop: null,
+  };
+
+  return fetch(
+    `${config.endpoint}/openai/deployments/${config.deployment}/completions?api-version=2022-12-01`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "api-key": config.apiKey,
+      },
+      body: JSON.stringify(data),
+    }
+  );
+}
+
+function queryOpenAIChat(text, config) {
+  const data = {
+    messages: `[{"role":"system","content":"${chatContext}"},{"role":"user","content":"${chatQuery}\n${text}"}]`,
+    max_tokens: getMaxTokens(text, config.deployment),
+    temperature: Number(config.temperature),
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    top_p: 0.95,
+    stop: null,
+  };
+
+  return fetch(
+    `${config.endpoint}/openai/deployments/${config.deployment}/chat/completions?api-version=2023-03-15-preview`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "api-key": config.apiKey,
+      },
+      body: JSON.stringify(data),
+    }
+  );
+}
+
+function buildQuery(text, config) {
+  if (
+    config.deployment === "code-davinci-002" ||
+    config.deployment === "text-davinci-003"
+  ) {
+    return queryOpenAICompletion(text, config);
+  } else {
+    return queryOpenAIChat(text, config);
+  }
+}
+
+function processOpenAIResponse(response) {
+  const choice = response.choices[0];
+  if (choice.text) {
+    return choice.text;
+  } else {
+    return choice.message.content;
+  }
+}
